db9a9a99dfae163a7872368bd20ea92856676d50
Rob Barry <rob.barry@ons.gov.uk>
2023-01-17 15:54:19 +0000

    Please?

--- csvlint-csv2rdf-sparqltests.ps1
+++ csvlint-csv2rdf-sparqltests.ps1
@@ -18,7 +18,7 @@ Expand-Archive -LiteralPath curl.zip -DestinationPath .
 cp curl-7.86.0-win64-mingw\bin\libcurl-x64.dll curl-7.86.0-win64-mingw\bin\libcurl.dll 
 cp curl-7.86.0-win64-mingw\bin\* C:\Ruby24-x64\bin\
 
-C:\Ruby24-x64\bin\gem.exe install bundler -v '2.3'
+gem install bundler -v '2.3'
 
 bundle show
 bundle init
