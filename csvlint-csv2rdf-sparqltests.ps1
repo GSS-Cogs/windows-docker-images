f2d314e81ba9f86fc79b644136b9790f474e7e4d
Rob Barry <rob.barry@ons.gov.uk>
2023-01-17 12:52:30 +0000

    Let's try this.

--- csvlint-csv2rdf-sparqltests.ps1
+++ csvlint-csv2rdf-sparqltests.ps1
@@ -2,6 +2,8 @@
 This script is used to install csv2rdf, csvlint and the sparql-test-runner inside the Windows environment of a GitHub Action Runner.  
 """
 
+$ErrorActionPreference = "Stop"
+
 $path = $env:PATH
 
 $initialWorkingDir = $pwd
@@ -14,9 +16,8 @@ cd csvlint
 Invoke-WebRequest -Uri https://curl.se/windows/dl-7.86.0/curl-7.86.0-win64-mingw.zip -OutFile "curl.zip"
 Expand-Archive -LiteralPath curl.zip -DestinationPath .
 cp curl-7.86.0-win64-mingw\bin\libcurl-x64.dll curl-7.86.0-win64-mingw\bin\libcurl.dll 
-cp curl-7.86.0-win64-mingw\bin\* C:\hostedtoolcache\windows\Ruby\2.4.10\x64\bin
+cp curl-7.86.0-win64-mingw\bin\* C:\Ruby24-x64\bin\
 
-gem install bundle
 bundle init
 bundle add i18n --version "~>1.12.0"
 bundle add csvlint --git https://github.com/GSS-Cogs/csvlint.rb --ref v0.6.7
@@ -29,7 +30,7 @@ bundle install
 
 $csvLintInstallationFolder = (Get-Item bin | Resolve-Path).Path.Substring(38)
 
-Set-Content -Path "$csvLintInstallationFolder\csvlint.bat" -Value "@REM Forwarder script`n@echo off`necho Attempting to launch csvlint`nC:\hostedtoolcache\windows\Ruby\2.4.10\x64\bin\ruby $csvLintInstallationFolder\csvlint %*"
+Set-Content -Path "$csvLintInstallationFolder\csvlint.bat" -Value "@REM Forwarder script`n@echo off`necho Attempting to launch csvlint`nC:\Ruby24-x64\bin\ruby.exe $csvLintInstallationFolder\csvlint %*"
 
 $path += ";$csvLintInstallationFolder"
 
@@ -42,7 +43,6 @@ $csv2rdfPath = (Get-Item csv2rdf.jar | Resolve-Path).Path.Substring(38)
 Set-Content -Path csv2rdf.bat -Value "@REM Forwarder script`n@echo off`necho Attempting to launch csv2rdf at $csv2rdfPath`njava -jar $csv2rdfPath %*" 
 
 $csv2rdfLocation = (Get-Item csv2rdf.bat | Resolve-Path).Path.Substring(38)
-echo "CSV2RDF_LOCATION=$csv2rdfLocation" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf8 -Append
 Write-Output = "csv2rdf location: $csv2rdfLocation"
 
 $path += ";$pwd"
@@ -60,8 +60,5 @@ git clone --depth 1 https://github.com/GSS-Cogs/gdp-sparql-tests.git
 
 $sparqlTestsLocation = (Get-Item gdp-sparql-tests/tests | Resolve-Path).Path.Substring(38)
 Write-Output = "Sparql test location: $sparqlTestsLocation"
-echo "SPARQL_TESTS_LOCATION=$sparqlTestsLocation" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf8 -Append
 
-Write-Output "Setting path"
-echo "PATH=$path" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
 Write-Output $path
